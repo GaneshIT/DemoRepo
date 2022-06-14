2ddbd7910d5a1f1c7a4d565a544dbf3d8912c287
Ganesan P <ganesh.ceoit@gmail.com>
2022-06-14 11:23:07 +0530

    Update CustomerData.cs
    updated

--- CustomerData.cs
+++ CustomerData.cs
@@ -1,11 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;//connect sql server
 using System.Data;//DataTable,DataSet
-
+//DataTable: Row and Column
+//DataSet: Multiple table
 namespace CustomerDbConsole
 {
     //ADO.NET - Connected mode(connection open and close) and disconnected mode
@@ -33,25 +34,75 @@ namespace CustomerDbConsole
             SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
             SqlCommand cmd = new SqlCommand("insert into Customer values("+id+",'"+name+"','"+email+"',"+mobile+",'"+address+"')", sqlConnection);
             sqlConnection.Open();//connection state is open
-            cmd.ExecuteNonQuery();//execute my sql commands
+          int result=  cmd.ExecuteNonQuery();//execute my sql commands 1
             sqlConnection.Close(); //connection state is close
+            if (result == 0)
+                return "Not inserted";
             return "Inserted";
         }
         public string UpdateCustomer()
         {
-            return "";
+            Console.Write("Enter Customer Id to update: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Customer name  to update: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Customer email  to update: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter Customer Mobile  to update: ");
+            int mobile = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Customer address  to update: ");
+            string address = Console.ReadLine();
+
+            //insert customer data into sqlserver
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            SqlCommand cmd = new SqlCommand("update Customer set CustName='" + name + "' , Email='" + email + "' , Mobile=" + mobile + " , CustAddress='" + address + "' where custid="+id+"", sqlConnection);
+            sqlConnection.Open();//connection state is open
+            int result = cmd.ExecuteNonQuery();//execute my sql commands 1
+            sqlConnection.Close(); //connection state is close
+            if (result == 0)
+                return "Not Updated";
+            return "Updated";
         }
-        public string DeleteCustomer()
+        public string DeleteCustomer(int custId)
         {
-            return "";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            SqlCommand cmd = new SqlCommand("delete from Customer where custid="+ custId, sqlConnection);
+            sqlConnection.Open();//connection state is open
+            int result = cmd.ExecuteNonQuery();//execute my sql commands 1
+            sqlConnection.Close(); //connection state is close
+            if (result == 0)
+                return "Not Deleted";
+            return "Deleted";
         }
-        public string SelectCustomers()
+        public DataTable SelectCustomers()
         {
-            return "";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            string db = sqlConnection.Database;
+            SqlCommand cmd = new SqlCommand("select * from Customer", sqlConnection);
+            sqlConnection.Open();//connection state is open
+            SqlDataReader dataReader = cmd.ExecuteReader();//execute select statment
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            //DataTable, DataSet
+            sqlConnection.Close(); //connection state is close
+            return dataTable;
         }
-        public string SelectCustomerById()
+        public DataTable SelectCustomerById(int custId)
         {
-            return "";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionStr);//connection establishment
+            string db = sqlConnection.Database;
+            SqlCommand cmd = new SqlCommand("select * from Customer where custid="+custId, sqlConnection);
+            sqlConnection.Open();//connection state is open
+            SqlDataReader dataReader = cmd.ExecuteReader();//execute select statment
+            DataTable dataTable = new DataTable();
+            dataTable.Load(dataReader);
+            //DataTable, DataSet
+            sqlConnection.Close(); //connection state is close
+            return dataTable;
         }
     }
 }
